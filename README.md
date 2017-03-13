7de0c61d33d8db5173ebf8d451e2c23d3b4e6697
Peter Schmalfeldt <peter@manifestinteractive.com>
2017-03-13 04:47:53 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# mysql-to-sequelize-seeder
-Use PHP to Connect to MySQL and Generate Sequelize Seeder File
+![Civil Services Logo](https://cdn.civil.services/common/github-logo.png "Civil Services Logo")
+
+Civil Services API
+===
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](https://raw.githubusercontent.com/CivilServiceUSA/geojson-to-mysql/master/LICENSE) [![GitHub contributors](https://img.shields.io/github/contributors/CivilServiceUSA/geojson-to-mysql.svg)](https://github.com/CivilServiceUSA/geojson-to-mysql/graphs/contributors)
+
+__Civil Services__ is a collection of tools that make it possible for citizens to be a part of what is happening in their Local, State & Federal Governments.
+
+
+MySQL to Sequelize Seeder
+---
+
+__IMPORTANT__:  This is an internal tool with a very specific function for Civil Services that exports GeoJSON data from MySQL tables to create Sequelize Seeder Files.
+
+Use PHP to Connect to MySQL and Generate Sequelize Seeder File.
+
+Copy `config.ini.dist` to `config.ini` and add your own values to it.
+
+#### Usage:
+
+```bash
+php /path/to/export.php --table zipcodes --order zipcode --sort ASC --where "zipcode like '99%'" --output "./app/seeders/20170301000000-shape-zipcode-99-seeder.js"
+```
+
---
+++ export.php
@@ -0,0 +1,129 @@
+<?php
+
+$config = parse_ini_file(realpath(dirname(__FILE__)) . '/config.ini');
+$options = getopt("", array('table:', 'limit:', 'where:', 'offset:', 'order:', 'sort:', 'output:'));
+
+define('DB_HOST', $config['host']);
+define('DB_USER', $config['username']);
+define('DB_PASS', $config['password']);
+define('DB_NAME', $config['dbname']);
+define('DB_TABLE', isset($options['table']) ? $options['table'] : null);
+define('EXPORT_PATH', isset($options['output']) ? $options['output'] : null);
+define('LIMIT', isset($options['limit']) ? $options['limit'] : null);
+define('OFFSET', isset($options['offset']) ? $options['offset'] : null);
+define('ORDER_BY', isset($options['order']) ? $options['order'] : null);
+define('SORT', isset($options['sort']) ? $options['sort'] : null);
+define('WHERE', isset($options['where']) ? $options['where'] : null);
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (empty(DB_TABLE)) {
+    exit('× Missing Required --table parameter' . PHP_EOL);
+}
+
+if (empty(EXPORT_PATH)) {
+    exit('× Missing Required --output parameter' . PHP_EOL);
+}
+
+// Make sure we have a MySQL Connection
+if ($mysqli) {
+
+    $query = "SELECT *, ST_AsText(shape) as shape FROM `" . DB_TABLE . "`";
+
+    if (!empty(WHERE)) {
+        $query .= " WHERE " . WHERE;
+    }
+
+    if (!empty(ORDER_BY)) {
+        $sort = (!empty(SORT)) ? SORT : 'ASC';
+        $query .= " ORDER BY " . ORDER_BY . ' ' . $sort;
+    }
+
+    if (!empty(LIMIT)) {
+        $query .= " LIMIT " . LIMIT;
+    }
+
+    if (!empty(OFFSET)) {
+        $query .= " OFFSET " . OFFSET;
+    }
+
+    $update_columns = array();
+    $insert = array();
+
+    if ($result = $mysqli->query($query)) {
+
+        while ($field = mysqli_fetch_field($result)) {
+            if ($field->name !== 'id' && $field->name !== 'created_date' && $field->name !== 'deleted_at') {
+                $update_columns[] = $field->name;
+            }
+        }
+
+        while ($row = $result->fetch_assoc()) {
+            $data = array();
+            for ($i = 0; $i < count($update_columns); $i++) {
+                if ($update_columns[$i] !== 'modified_date') {
+                    $data[$update_columns[$i]] = $row[$update_columns[$i]];
+                }
+
+                if ($update_columns[$i] === 'shape') {
+                    $data[$update_columns[$i]] = 'queryInterface.sequelize.fn(\'GeomFromText\', \'' . $row[$update_columns[$i]] . '\')';
+                }
+            }
+
+            $data['created_date'] = "new Date()";
+            $data['modified_date'] = "new Date()";
+
+            $insert[] = json_encode($data, JSON_PRETTY_PRINT);
+        }
+
+        $columns = join("','", $update_columns);
+        $json = join(",", $insert);
+
+        $json = str_replace('"new Date()"', 'new Date()', $json);
+        $json = str_replace('"created_date"', 'created_date', $json);
+        $json = str_replace('"queryInterface', 'queryInterface', $json);
+        $json = str_replace('\')",', '\'),', $json);
+
+        for ($i = 0; $i < count($update_columns); $i++) {
+            $json = str_replace('"' . $update_columns[$i] . '"', $update_columns[$i], $json);
+        }
+
+        $json = str_replace('"', "'", $json);
+
+        $seeder = <<<EOD
+module.exports = {
+  up: function (queryInterface) {
+    return queryInterface.bulkInsert('{$options['table']}', [
+        {$json}
+    ], {
+      updateOnDuplicate: ['{$columns}']
+    }).catch(function (err) {
+      if (err && err.errors) {
+        for (var i = 0; i < err.errors.length; i++) {
+          console.error('× SEED ERROR', err.errors[i].type, err.errors[i].message, err.errors[i].path, err.errors[i].value);
+        }
+      } else if (err && err.message) {
+        console.error('× SEED ERROR', err.message);
+      }
+    });
+  },
+  down: function (queryInterface) {
+    return queryInterface.bulkDelete('{$options['table']}', null, {});
+  }
+};
+EOD;
+
+        file_put_contents(EXPORT_PATH, $seeder);
+
+    } else {
+        printf("× %s\n", $mysqli->error);
+    }
+
+    /* Close Connection */
+    $mysqli->close();
+} else {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
